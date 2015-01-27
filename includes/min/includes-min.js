ef1bcbf201464183c26ec974313de60207d41b75
J Holly <jholly1@tnstate.edu>
2015-01-27 14:41:08 -0600

    Separated SASS variables into separate SASS file. Created DRYer implementation for header. Minor content edits.

---
+++ includes/min/includes-min.js
@@ -0,0 +1,1 @@
+$(function(){$("section.utility-nav").load("includes/header/utility.html"),$("section.primary-nav").load("includes/header/primarynav.html"),$("section.secondary-nav").load("includes/header/secondary.html")});
